b781db380081b7990033d6b65ce23eef2b9b1aa1
Sangeetha21 <sangeetha.gopal@a-cti.com>
2021-09-12 15:31:26 +0530

    first folder

---
+++ test/exampleTest.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+describe("Color Code Converter", function() {
+    describe("RGB to Hex conversion", function() {
+      
+    });
+});
+  // "test": "mocha test/*.js --reporter spec --reporter mocha-allure-reporter; npm run report",
+    // "report": "allure generate allure-results --clean -o allure-report && allure open allure-report"
