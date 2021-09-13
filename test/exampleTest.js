cb4b9eb32629c0e797c880664155b3910cf7a5a0
Sangeetha21 <sangeetha.gopal@a-cti.com>
2021-09-13 20:58:22 +0530

    added reports to run sample code

--- test/exampleTest.js
+++ test/exampleTest.js
@@ -1,8 +1,30 @@
+
 var chai = require('chai');
-describe("Color Code Converter", function() {
-    describe("RGB to Hex conversion", function() {
-      
+require("mocha-allure-reporter");
+
+var XMLHttpRequest = require('xhr2');
+// var xhr = new XMLHttpRequest();
+//  var fetch = require('node-fetch').default;
+
+describe("Color Code Converter", function () {
+  it("changes", function () {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        // allure.description("this responseText");
+      }
     });
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+
+    xhr.send();
+    allure.description("this responseText");
+
+
+  });
 });
+
   // "test": "mocha test/*.js --reporter spec --reporter mocha-allure-reporter; npm run report",
     // "report": "allure generate allure-results --clean -o allure-report && allure open allure-report"
